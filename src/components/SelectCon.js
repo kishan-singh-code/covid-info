af3a0b387cc8d538570509be4f63ae9a3ae8275d
Kishan Singh <kishansingh3d21f@gmail.com>
2022-04-09 17:59:06 +0530

    090422_5:58

--- src/components/SelectCon.js
+++ src/components/SelectCon.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./css/SelectCon.css";
-import { Link } from "react-router-dom";
 import olol from "./img/world.png"
 import { prettyPrintStat } from "./utils";
 
